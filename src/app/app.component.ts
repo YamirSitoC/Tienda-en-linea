151d44cf2b97c371aa028eaa53d1d512fd68c69b
YamirSitoC <castroyamir123@gmail.com>
2023-09-07 19:57:27 -0700

    Creacion del frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+
+declare var  $:any;
+declare function initianPageTiendaOnline([]):any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tiendaOnline';
+
+  constructor(){
+    setTimeout(() => {
+      initianPageTiendaOnline($);
+    }, 50);
+  }
 }
